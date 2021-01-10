b02196fec0fdd32eb090ebc7156c4560feb4fd04
Anton Kats <antonkats@gmail.com>
2021-01-10 07:16:31 -0400

    Improved types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,24 @@
-export type lcConstructor<T extends object> = (data: object) => lcInstance<T>
-
 export type lcInstance<T extends object> = {
     [key in keyof T]: T[key]
 }
+export type lcLayerConstructor<S, M> = (($: S, d: any) => M) | (($: S, d: any) => () => {}) | M
+export type lcConstructor<M extends object> = (data: object | undefined) => lcInstance<M>
 
-export default function layerCompose <T extends {}>(...layers): lcConstructor<T>
+type OrEmptyObject<T> = T extends {} ? T : {}
+type O<T> = OrEmptyObject<T>
+
+declare function layerCompose<M1, M2, M3>(s1: lcLayerConstructor<{}, O<M1>>, s2: lcLayerConstructor<O<M1>, O<M2>>,
+                                          s3: lcLayerConstructor<O<M1> & O<M2>, O<M3>>,): lcConstructor<O<M1>&O<M2>&O<M3>>;
+declare function layerCompose<M1, M2>(s1: lcLayerConstructor<{}, O<M1>>,
+                                      s2: lcLayerConstructor<O<M1>, O<M2>>,): lcConstructor<O<M1>&O<M2>>;
+declare function layerCompose<M1>(s1: lcLayerConstructor<{}, O<M1>>): lcConstructor<O<M1>>;
+
+declare function layerCompose(...layers: lcLayerConstructor<any, any>): lcConstructor<any>;
+
+export default layerCompose
+
 
 /* utils */
 export function unbox(what: lcInstance<any>): object | undefined
+
+export const IS_DEV_MODE: boolean
