92ef3781e8a430490d67dbbb7f20de56fb6eb689
Anton Kats <antonkats@gmail.com>
2021-07-27 14:40:27 -0300

    feat: pausable Observable

--- src/external/compositions/Observable.js
+++ src/external/compositions/Observable.js
@@ -1,26 +1,49 @@
-import layerCompose from '../../layerCompose';
+import layerCompose from '../../layerCompose'
 import Async        from "./Async"
+import defaults     from "../patterns/defaults"
 
-const $owner = Symbol();
+const $owner = Symbol()
 const $assign = Symbol()
 
 /**
- * Notifies all layers of the current object that a write has been performed to the internal interface (which usually would be a POJO)
+ * Notifies all layers of the current object that a write has been performed to the internal interface (which usually
+ * would be a POJO)
  * */
 
 export default layerCompose(
     Async,
 
     ($, _) => {
-        _(makeProxy);
-        $._attachObservable();
+        _(makeProxy)
+        $._attachObservable()
+    },
+
+    {
+        Observable: [
+            $ => $(Owner => ({ Owner })),
+            defaults({ paused: false }, true),
+            {
+                pause($, _) {
+                    _.paused = true
+                },
+                resume($, _) {
+                    _.paused = false
+                },
+                onUpdate($, _) {
+                    if (!_.paused) {
+                        $.Owner.onUpdate()
+                    }
+                },
+            },
+        ]
     },
 
     {
         _attachObservable($, _) {
-            _[$owner] = $;
+            if ("onUpdate" in $) {
+                _[$owner] = $
+            }
         },
-
         then($, _) {
             /* check if has `then` */
             if ("then" in _ && typeof _.then == "function") {
@@ -29,8 +52,7 @@ export default layerCompose(
             }
         }
     },
-
-);
+)
 
 export function isObservable(obj) {
     return obj[$owner]
@@ -44,18 +66,18 @@ function makeProxy(proxyTarget) {
         set(target, prop, val) {
 
             const hasChanged = target[prop] !== val
-            target[prop] = val;
+            target[prop] = val
 
             if (hasChanged && typeof prop != 'symbol' && prop[0] !== "_") {
-                const owner = proxyTarget[$owner];
-                if (owner && "onUpdate" in owner) {
-                    owner.onUpdate({
+                const owner = proxyTarget[$owner]
+                if (owner) {
+                    owner.Observable.onUpdate({
                         prop,
                         val
-                    });
+                    })
                 }
             }
-            return true;
+            return true
         }
-    });
+    })
 }
