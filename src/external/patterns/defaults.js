95f5ff05296348b253e12fd08072716bf44ffb20
Anton Kats <antonkats@gmail.com>
2021-08-11 15:42:47 -0300

    fix: defaults wasn't generating if a function had more than 0 args

--- src/external/patterns/defaults.js
+++ src/external/patterns/defaults.js
@@ -5,7 +5,15 @@ import {$isCompositionInstance, IS_DEV_MODE} from "../../const"
  * @param overwriteToPojo if the inner interface is not a POJO, overwrite it to be
  * */
 export default function (presetValues, overwriteToPojo = false) {
+    if (typeof overwriteToPojo == "object") {
+        return defaults(presetValues, overwriteToPojo)
+    } else {
+        console.warn("You're using legacy form of `defaults`. Switch to using an object as the second argument {[overwriteToPojo], [generate]}")
+        return defaults(presetValues, { overwriteToPojo, generate: false })
+    }
+}
 
+function defaults (presetValues, {overwriteToPojo= false, doGenerate= true} = {overwriteToPojo: false, doGenerate: true}) {
     return ($, _) => _(initialCore => {
         let core = overwriteToPojo && initialCore[$isCompositionInstance] ? {} : initialCore
 
@@ -19,8 +27,12 @@ export default function (presetValues, overwriteToPojo = false) {
                     }
                 }
                 let v = presetValues[k]
-                if (typeof v == "function" && v.length === 0) {
-                    v = v(initialCore)
+                if (typeof v == "function" && v.length < 2) {
+                    if (v.length === 0) {
+                        v = v()
+                    } else if (v.length < 2 && doGenerate) {
+                        v = v(initialCore)
+                    }
                     // truthy check for in case of null
                 } else if (!!v && typeof v == "object") {
                     throw new Error(`Raw objects are not allowed as defaults (key: ${k}). They will carry over to other instances. Use \`() => ...\` to generate them dynamically`)
