f5abba263c9750b501723e48cc0f709d6cee8008
Anton Kats <antonkats@gmail.com>
2021-11-02 15:23:20 -0300

    change: disallow functions as core objects

--- src/constructor/constructCoreObject.js
+++ src/constructor/constructCoreObject.js
@@ -1,22 +1,22 @@
 import {$isNullCore, IS_DEV_MODE} from "../const"
 
-export default function constructCoreObject(proposed, $) {
+export default function constructCoreObject(proposed) {
     let core
-    if (typeof proposed === "function") {
-        core = proposed($)
-    } else if (proposed == null) {
+
+    if (proposed == null) {
         core = {}
         if (IS_DEV_MODE) {
             core[$isNullCore] = true
         }
-        // todo. add a DEV marker that this is a null core for single-use lenses/services
     } else {
         core = proposed
+
+        // Promises are objects
+        if (typeof core !== "object") {
+            throw new Error('Data must be an object (not a primitive)')
+        }
     }
 
-    if (typeof core !== "object") {
-        throw new Error('Data must be an object (not a primitive)')
-    }
 
     return core
 }
