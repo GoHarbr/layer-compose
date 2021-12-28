b8628f11173afa7e817683c30064c15076be8a5c
Anton Kats <antonkats@gmail.com>
2021-12-28 16:08:58 -0400

    feature: replace

--- src/external/patterns/replace.js
+++ src/external/patterns/replace.js
@@ -1,14 +1,12 @@
 import {$dataPointer, $isCompositionInstance, IS_DEV_MODE} from "../../const"
 
-export default function replace(valuesOrGenerator) {
-    return ($, _) => {
+export default function replace($, replaceCoreWith) {
+        if (replaceCoreWith) {
+            if (replaceCoreWith[$isCompositionInstance]) throw new Error('Cannot replace _ with a Composition')
+            if (Object.keys($[$dataPointer]).length) throw new Error('Cannot replace a non-empty core (_ == {} must be true)')
 
-        const values = typeof valuesOrGenerator == 'function' ? valuesOrGenerator($, _) : valuesOrGenerator
-
-        if (values) {
-            $[$dataPointer] = values
+            $[$dataPointer] = replaceCoreWith
         } else {
             if (IS_DEV_MODE) console.warn("Replace action had no effect")
         }
-    }
 }
