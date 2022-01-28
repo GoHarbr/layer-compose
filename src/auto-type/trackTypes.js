f749fd2982d2114a8374538410a372e0cee63d58
Anton Kats <antonkats@gmail.com>
2022-01-28 17:27:29 -0400

    fix: not returning generated flow type

--- src/auto-type/trackTypes.js
+++ src/auto-type/trackTypes.js
@@ -26,7 +26,7 @@ function flowRepresentationFor$(types) {
                 }
 
 
-    `: { (coreUpdate: {}): void, [key : ${lenses.map(k => `'${k}'`).join('|')}] : ((coreUpdate: {}, cb: ?(lens) => void) => void) | (cb: (lens) => void) => void), [key : ${methods.map(k => `'${k}'`).join('|')}] : (o: ?any) => {}, [key : ${accessors.map(k => `'${k}'`).join('|')}] : any }`
+    return `: { (coreUpdate: {}): void, [key : ${lenses.map(k => `'${k}'`).join('|')}] : ((coreUpdate: {}, cb: ?(lens) => void) => void) | (cb: (lens) => void) => void), [key : ${methods.map(k => `'${k}'`).join('|')}] : (o: ?any) => {}, [key : ${accessors.map(k => `'${k}'`).join('|')}] : any }`
 }
 
 export function writeTypesToDisk() {
