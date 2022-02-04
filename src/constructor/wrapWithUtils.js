17c9fd8435c6de1309dcd7d3d0db6c6b8203e2dd
Anton Kats <antonkats@gmail.com>
2022-02-04 14:38:26 -0400

    feature: global mocking

--- src/constructor/wrapWithUtils.js
+++ src/constructor/wrapWithUtils.js
@@ -1,5 +1,15 @@
 import lens from "../external/patterns/lens"
+import { $at, $layers } from "../const"
 
 export function wrapWithUtils(constructor) {
-    constructor.lens = ($, applicator) => lens($, applicator, constructor)
+    /** @deprecated */
+    constructor.lens = ($, applicator) => {
+        console.warn('lens() is deprecated. Use getters instead')
+        lens($, applicator, constructor)
+    }
+
+    constructor.mock = (...layers) => {
+        layers[$at] = new Error()
+        constructor[$layers] = layers
+    }
 }
