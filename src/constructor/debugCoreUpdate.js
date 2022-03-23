304d286dd0a39fb2ac2d7ebd53b07e7371ec5901
Anton Kats <antonkats@gmail.com>
2022-03-23 09:42:55 -0300

    feature: add tracing id in debug output

--- src/constructor/debugCoreUpdate.js
+++ src/constructor/debugCoreUpdate.js
@@ -1,11 +1,11 @@
 import { GLOBAL_DEBUG } from "../external/utils/enableDebug"
-import { $fullyQualifiedName, $tag } from "../const"
+import { $fullyQualifiedName, $tag, $traceId } from "../const"
 import { findLocationFromError } from "../external/utils/findLocationFromError"
 
 export default function ($) {
     if (GLOBAL_DEBUG.enabled) {
         const at = new Error()
-        const header = `()   ${$[$fullyQualifiedName] || $[$tag] || ''}`
+        const header = `()   ${$[$fullyQualifiedName] || $[$tag] || ''} [${$[$traceId]}]`
         console.debug(`${header.padEnd(95)} :: ${findLocationFromError(at)}`)
     }
 }
